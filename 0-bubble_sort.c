68628396d3f13bb058c07e7a0672fff7110aa4aa
niialabi <taufiq.alabi@gmail.com>
2023-09-23 20:17:06 +0000

    petty betty fix - bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -7,22 +7,22 @@
 */
 void bubble_sort(int *array, size_t size)
 {
-        size_t i, j;
-        int temp;
+	size_t i, j;
+	int temp;
 
-        if (size <= 1)
-                return;
-        for (i = 0; i < size; i++)
-        {
-                for (j = 0; j < size - 1; j++)
-                {
-                        if (array[j] > array[j + 1])
-                        {
-                                temp = array[j];
-                                array[j] = array[j + 1];
-                                array[j + 1] = temp;
-                                print_array(array, size);
-                        }
-                }
-        }
+	if (size <= 1)
+		return;
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				print_array(array, size);
+			}
+		}
+	}
 }
