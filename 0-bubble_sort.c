7d871fdd36c7e2761b8aca4fb0f969c89144c4c5
niialabi <taufiq.alabi@gmail.com>
2023-09-23 20:11:40 +0000

    bubble sort clean up

---
+++ 0-bubble_sort.c
@@ -0,0 +1,28 @@
+#include "sort.h"
+#include <stdio.h>
+/**
+ * bubble_sort - sorts dt using bubble sort alg.
+ * @array: pointer to array to sort
+ * @size: size of array
+*/
+void bubble_sort(int *array, size_t size)
+{
+        size_t i, j;
+        int temp;
+
+        if (size <= 1)
+                return;
+        for (i = 0; i < size; i++)
+        {
+                for (j = 0; j < size - 1; j++)
+                {
+                        if (array[j] > array[j + 1])
+                        {
+                                temp = array[j];
+                                array[j] = array[j + 1];
+                                array[j + 1] = temp;
+                                print_array(array, size);
+                        }
+                }
+        }
+}
